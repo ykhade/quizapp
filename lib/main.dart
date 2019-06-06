5e2e436fd18c5c0dba012e82efb58f994692b7ca
yashkhade <ykhade@nevada.unr.edu>
2019-06-05 22:25:01 -0700

    Stateful vs Stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,22 +7,48 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: new AwesomePage(),
+        body: new MyFirstWidget(
+          color: Colors.blue,
+        ),
       ),
     );
   }
 }
 
-class AwesomePage extends StatelessWidget {
-  const AwesomePage({
-    Key key,
-  }) : super(key: key);
+class MyFirstWidget extends StatefulWidget {
+  final Color color;
+
+  const MyFirstWidget({Key key, this.color = Colors.red}) : super(key: key);
+
+  @override
+  _MyFirstWidgetState createState() => _MyFirstWidgetState();
+}
+
+class _MyFirstWidgetState extends State<MyFirstWidget> {
+  int count = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        child: Text('Hello World!'),
+      child: FlatButton(
+        child: Text('$count'),
+        onPressed: () {
+          setState(() {
+            count++;
+          });
+        },
       ),
     );
   }
