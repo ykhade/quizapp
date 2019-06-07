25f00d121882104ee2ed09658045b37100615eee
yashkhade <ykhade@nevada.unr.edu>
2019-06-06 22:28:40 -0700

    Animated Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'dart:ui' as prefix0;
 
 import 'package:flutter/material.dart';
@@ -6,35 +7,53 @@ import 'dart:io' show Platform;
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  ScrollController _controller = ScrollController();
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(brightness: Brightness.dark),
       home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => _controller.animateTo(1000,
-              duration: Duration(seconds: 2), curve: Curves.bounceIn),
-        ),
-        body: ListView.builder(
-          controller: _controller,
-          itemBuilder: (context, idx) {
-            return Container(
-              color: Colors.orangeAccent,
-              margin: EdgeInsets.all(20),
-              height: 100,
-              child: Text('$idx'),
-            );
-          },
-        ),
-      ),
+          body: Center(
+        child: CoolBox(),
+      )),
     );
+  }
+}
+
+class CoolBox extends StatefulWidget {
+  const CoolBox({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _CoolBoxState createState() => _CoolBoxState();
+}
+
+class _CoolBoxState extends State<CoolBox> {
+  double width = 100;
+  double height = 100;
+  Color color = Colors.green;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+        duration: Duration(seconds: 2),
+        curve: Curves.bounceInOut,
+        color: color,
+        width: width,
+        height: height,
+        child: FlatButton(
+          child: Text('Random'),
+          onPressed: () {
+            setState(() {
+              width = Random().nextDouble() * 400;
+              height = Random().nextDouble() * 400;
+
+              int r = Random().nextInt(255);
+              int b = Random().nextInt(255);
+              int g = Random().nextInt(255);
+              color = Color.fromRGBO(r, b, g, 1);
+            });
+          },
+        ));
   }
 }
