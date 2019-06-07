99b381690e3a7eccca260e10a232da0b014d5b19
yashkhade <ykhade@nevada.unr.edu>
2019-06-06 17:14:32 -0700

    Smooth scrolling unlocked

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,39 @@
+import 'dart:ui' as prefix0;
+
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io' show Platform;
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ScrollController _controller = ScrollController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
       home: Scaffold(
-        body: new MyFirstWidget(
-          color: Colors.blue,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => _controller.animateTo(1000,
+              duration: Duration(seconds: 2), curve: Curves.bounceIn),
         ),
-      ),
-    );
-  }
-}
-
-class MyFirstWidget extends StatefulWidget {
-  final Color color;
-
-  const MyFirstWidget({Key key, this.color = Colors.red}) : super(key: key);
-
-  @override
-  _MyFirstWidgetState createState() => _MyFirstWidgetState();
-}
-
-class _MyFirstWidgetState extends State<MyFirstWidget> {
-  int count = 0;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: FlatButton(
-        child: Text('$count'),
-        onPressed: () {
-          setState(() {
-            count++;
-          });
-        },
+        body: ListView.builder(
+          controller: _controller,
+          itemBuilder: (context, idx) {
+            return Container(
+              color: Colors.orangeAccent,
+              margin: EdgeInsets.all(20),
+              height: 100,
+              child: Text('$idx'),
+            );
+          },
+        ),
       ),
     );
   }
